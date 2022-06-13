f70cbc234d7edcbced2c2e0c81ae24b2df7df0a7
jessie1003123 <109703060@nccu.edu.tw>
2022-06-13 15:47:28 +0800

    2 files added

---
+++ 0613test/0613/0613.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-0613',
+  templateUrl: './0613.component.html',
+  styleUrls: ['./0613.component.css']
+})
+export class 0613Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
